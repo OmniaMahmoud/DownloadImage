23f86ca89104c06bbe3cd8789bb91c772ee5a668
OmniaMahmoud <omnia.rezeka@yahoo.com>
2017-06-22 13:57:06 +0200

    Initial commit

---
+++ app/src/main/java/omnia/downloadimage/DownloadImage.java
@@ -0,0 +1,78 @@
+package omnia.downloadimage;
+
+import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Lenovo-pc on 22/06/2017.
+ */
+
+public class DownloadImage extends Fragment implements Runnable{
+    View v;
+    EditText url;
+    String urlStr;
+    ImageView imageView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        v=inflater.inflate(R.layout.downloadimage,container,false);
+        url=(EditText)v.findViewById(R.id.url);
+        Button download=(Button)v.findViewById(R.id.download);
+        imageView=(ImageView)v.findViewById(R.id.imageView);
+        Log.e("jjjjj","jjjjjjj");
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Thread thread=new Thread(DownloadImage.this);
+                thread.start();
+            }
+        });
+
+        return v;
+    }
+//  http://images.all-free-download.com/images/graphiclarge/curious_baby_193909.jpg
+    @Override
+    public void run() {
+        Log.e("omnia","thread 1");
+        urlStr=url.getText().toString();
+        if(!urlStr.isEmpty()){
+            try {
+                URL imageUrl=new URL(urlStr);
+                HttpURLConnection httpURLConnection=(HttpURLConnection) imageUrl.openConnection();
+                httpURLConnection.connect();
+                InputStream inputStream=httpURLConnection.getInputStream();
+                final Bitmap image= BitmapFactory.decodeStream(inputStream);
+                inputStream.close();
+                httpURLConnection.disconnect();
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageView.setImageBitmap(image);
+                    }
+                });
+                Log.e("bitmap","finish");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/omnia/downloadimage/MainActivity.java
@@ -0,0 +1,19 @@
+package omnia.downloadimage;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentTransaction ft=getFragmentManager().beginTransaction();
+        ft.replace(R.id.main_frame,new DownloadImage());
+        ft.commit();
+    }
+
+}
